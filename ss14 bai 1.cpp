1b74e249255a346ff7700620118bb0b3c6d746a5
ChloeAmelinda <trangthichngunuong@gmail.com>
2024-12-11 22:35:06 +0700

    ss14 bai 1

--- ss14 bai 1.cpp
+++ ss14 bai 1.cpp
@@ -1,12 +1,11 @@
 #include<stdio.h>
 #include<string.h>
 int main(){
-	char c[1000];
-	printf("Nhap xau ky tu :");
-	scanf("%s",&c);
-	gets(c);
-	printf("Ky tu vua nhap la: %s\t",c);
-	printf("do dai : %d\n",strlen(c));
+	char c[50];
+		printf("Moi ban nhap vao chuoi ki tu:");
+		fgets(c,50,stdin);
+		printf("Ky tu vua nhap la:%s",c);
+		printf("Do dai : %d\n",strlen(c));
 	return 0;
 	
 }
